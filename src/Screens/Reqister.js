c717dbc0ea7ec1dd11db6236123cac68ca7adfa9
Rohman Cristal <zakariataufik8@gmail.com>
2023-11-11 13:20:49 +0700

    first commit

---
+++ src/Screens/Reqister.js
@@ -0,0 +1,48 @@
+import * as React from 'react';
+import {useState, useEffect} from 'react';
+import {View, Image, Text, TouchableOpacity} from 'react-native';
+
+const Reqister = () => {
+  return (
+    <View>
+      <Image
+        source={require('../aset/image/stan.jpg')}
+        style={{height: 200, width: 360}}
+      />
+      <Text
+        style={{
+          textAlign: 'center',
+          fontWeight: 'bold',
+          fontSize: 15,
+          marginTop: 50,
+          color: 'black',
+        }}>
+        Buatlah akun terlebih dahulu untuk terhubung ke apk psb,klik mulai di
+        bawah ini dan ikuti langkah langkahnya
+      </Text>
+      <TouchableOpacity
+        style={{
+          backgroundColor: 'green',
+          height: 30,
+          width: 100,
+          marginTop: 20,
+          marginHorizontal: 130,
+          borderRadius: 5,
+        }}>
+        <Text
+          style={{
+            textAlign: 'center',
+            fontSize: 15,
+            fontWeight: 'bold',
+            marginVertical: 2,
+            marginTop: 4,
+            color: 'white',
+          }}>
+          Mulai
+        </Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
+
+export default Reqister;
