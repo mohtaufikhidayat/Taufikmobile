e2ffd791c998aead8de2b0b6cb9a4653289056b3
haris <mohtaufikhidayat>
2023-11-11 17:48:45 -0800

    menambahkan halaman register

--- src/Screens/Reqister.js
+++ src/Screens/Reqister.js
@@ -1,43 +1,33 @@
 import * as React from 'react';
-import {useState, useEffect} from 'react';
-import {View, Image, Text, TouchableOpacity} from 'react-native';
+import { useState, useEffect } from 'react';
+import { View, Image, Text, TouchableOpacity, TextInput } from 'react-native';
 
 const Reqister = () => {
   return (
     <View>
       <Image
         source={require('../aset/image/stan.jpg')}
-        style={{height: 200, width: 360}}
+        style={{ height: 200, width: 360 }}
       />
-      <Text
-        style={{
+      <TextInput placeholder='MASUKKAN NAMA' style={{ elevation:2,backgroundColor:"white", marginHorizontal: 30, borderRadius: 10, marginBottom: 3, marginTop: 70, }} />
+      <TextInput placeholder='GENDER' style={{ backgroundColor: 'white',elevation:2, marginHorizontal: 30, borderRadius: 10, marginBottom: 3, marginTop: 20, }} />
+      <TextInput placeholder='BUAT EMAIL' style={{ backgroundColor: 'white',elevation:2, marginHorizontal: 30, borderRadius: 10, marginBottom: 3, marginTop: 20, }} />
+      <TextInput placeholder='BUAT PASSWORD' style={{ backgroundColor: 'white',elevation:2, marginHorizontal: 30, borderRadius: 10, marginBottom: 5, marginTop: 20, }} />
+
+      <TouchableOpacity style={{
+        backgroundColor: 'white',
+        marginTop: 30,
+        width: 80,
+        marginHorizontal: 140,
+        height: 30,
+        borderRadius: 10,elevation:3,
+      }}>
+        <Text style={{
           textAlign: 'center',
-          fontWeight: 'bold',
+          marginTop: 4,
           fontSize: 15,
-          marginTop: 50,
-          color: 'black',
-        }}>
-      </Text>
-      <TouchableOpacity
-        style={{
-          backgroundColor: 'green',
-          height: 30,
-          width: 100,
-          marginTop: 20,
-          marginHorizontal: 130,
-          borderRadius: 5,
-        }}>
-        <Text
-          style={{
-            textAlign: 'center',
-            fontSize: 15,
-            fontWeight: 'bold',
-            marginVertical: 2,
-            marginTop: 4,
-            color: 'white',
-          }}>
-          Mulai
-        </Text>
+          fontWeight: 'bold',
+        }}>Done</Text>
       </TouchableOpacity>
     </View>
   );
